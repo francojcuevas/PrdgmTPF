ed81bbf7fc8c6ac196d8dbeeff32e0cb0ba0e39f
Franco Cuevas <franco.j.cuevas@gmail.com>
2020-11-24 12:41:35 -0300

    initial

---
+++ src/ventasdao/controladores/ClienteControlador.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ventasdao.controladores;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import ventasdao.dominio.Conexion;
+import ventasdao.objetos.Cliente;
+
+/**
+ *
+ * @author Hugo Chanampe
+ */
+public class ClienteControlador implements ICrud<Cliente>{
+    
+    private Connection connection;
+    
+    private Statement stmt;
+    
+    private PreparedStatement ps;
+    
+    private ResultSet rs;
+    
+    private String sql;
+    
+    
+
+    //public void modificarCategoria(Categoria c);
+    //public Categoria obtenerCategoria(Integer id);
+    //public void eliminarCategoria(Categoria c);
+    
+    @Override
+    public boolean crear(Cliente entidad) throws SQLException, Exception{
+
+        connection = Conexion.obtenerConexion ();
+        String sql = "INSERT INTO clientes (nombre,apellido,cuil,fecha_nacimiento, tipo_cliente_id) VALUES (?,?,?,?,?)";
+        Date fecha = new Date(entidad.getFechaNacimiento().getTime());
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, entidad.getNombre());
+            ps.setString(2, entidad.getApellido ());
+            ps.setString(3, entidad.getCuil());
+            ps.setDate(4, fecha);
+            ps.setInt(5, entidad.getTipoCliente().getId());
+            ps.executeUpdate();
+            connection.close();
+            
+
+        } catch (SQLException ex) {
+            Logger.getLogger(CategoriaControlador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean eliminar(Cliente entidad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Cliente> listar() throws SQLException,Exception{
+        
+     connection = Conexion.obtenerConexion ();
+        try{
+            
+            this.stmt = connection.createStatement();
+            this.sql = "SELECT * FROM clientes";
+            this.rs   = stmt.executeQuery(sql);
+            connection.close();
+            
+            ArrayList<Cliente> clientes = new ArrayList();
+            
+            while(rs.next()){
+                
+                Cliente cliente = new Cliente();
+                
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setCuil(rs.getString("cuil"));
+                cliente.setId(rs.getInt("id"));
+                cliente.setApellido (rs.getString("apellido"));
+                cliente.setFechaNacimiento(rs.getDate("fecha_nacimiento"));
+                System.out.println(cliente);
+
+                clientes.add(cliente);
+                
+                
+            }
+            //System.out.println(cont);
+            return clientes;
+        } catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        return null;
+    
+
+    }
+
+    @Override
+    public boolean modificar(Cliente entidad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Cliente extraer(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+}
